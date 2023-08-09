a5cceffabd02b0a50f2695b432d686ca03ef628c
kingmckie <138835492+kingmckie@users.noreply.github.com>
2023-08-08 19:50:06 -0700

    Add files via upload

---
+++ FSDI 103_assignment1/variable.js
@@ -0,0 +1,7 @@
+let userName = "FlyGuy39";
+let passWord = "YourMommaHouse123";
+console.log(userName, passWord)
+document.write(`
+<p>${userName} is your username</p>
+<p>${passWord} is your password</p>
+`)
